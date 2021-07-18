43b41d1aa418d5808b05118b1276ec1ff04ff856
kakashy <45477236+kakashy@users.noreply.github.com>
2021-07-19 02:21:40 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
 import { windi } from 'svelte-windicss-preprocess';
 import WindiCSS from 'vite-plugin-windicss';
-// import vercel from '@sveltejs/adapter-vercel';
+import vercel from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [windi({})],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
-		// adapter: vercel()
+		adapter: vercel()
 	}
 };
 
